ae5a7c0d87baaeff069cf70029d0f5c3cf0521ea
annlita <annlitajoshy@gmail.com>
2025-04-02 09:48:01 -0400

    deploy2

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 import MySQLdb.cursors
 
-#mysql = MySQL(app)
 
 import pymysql
 
@@ -19,7 +18,8 @@ app.config['MYSQL_USER'] = Config.MYSQL_USER
 app.config['MYSQL_PASSWORD'] = Config.MYSQL_PASSWORD
 app.config['MYSQL_DB'] = Config.MYSQL_DB
 
-
+mysql = MySQL(app)
+cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
 # Flask-Mail setup (assuming you're using Flask-Mail for email sending)
 
